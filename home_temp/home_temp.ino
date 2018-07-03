2a6772da41752252d4dae9fc6178af711cc49b57
kpmehmetkeskin <mehmet.keskin@koctas.com.tr>
2018-07-03 16:34:15 +0300

    Kod düzenlendi.

--- home_temp/home_temp.ino
+++ home_temp/home_temp.ino
@@ -3,23 +3,20 @@
 
 #define ag_ismi "JAVA_HOME"
 #define ag_sifresi "avcz3463"
-#define IP "184.106.153.149"    //thingspeak.com IP adresi
+#define IP "184.106.153.149"
 
-#define DHTPIN 7     // what pin we're connected to
-#define DHTTYPE DHT22   // DHT 22  (AM2302)
-DHT dht(DHTPIN, DHTTYPE);
-
-
- 
+#define DHTPIN 7
+#define DHTTYPE DHT22
+  DHT dht(DHTPIN, DHTTYPE);
   float sicaklik;
- 
+  
   void setup()
   {
-  Serial.begin(115200);
-  dht.begin();
-  Serial.println("AT");
-  delay(1000); 
-  analogReference(INTERNAL);
+    Serial.begin(115200);
+    dht.begin();
+    Serial.println("AT");
+    delay(1000); 
+    analogReference(INTERNAL);
     if(Serial.find("OK")){
       Serial.println("AT+CWMODE=1"); 
       delay(1000);
@@ -28,15 +25,14 @@ DHT dht(DHTPIN, DHTTYPE);
     }
   }
  
-void loop(){
-  float sicaklik = dht.readTemperature();
- Serial.println(sicaklik);
- sicaklik_yolla(sicaklik);
- delay(10000);
-}
- 
- 
-void sicaklik_yolla(float sicaklik){
+  void loop(){
+    float sicaklik = dht.readTemperature();
+    Serial.println(sicaklik);
+    sicaklik_yolla(sicaklik);
+    delay(60000);
+  }
+   
+  void sicaklik_yolla(float sicaklik){
     Serial.println(String("AT+CIPSTART=\"TCP\",\"") + IP + "\",80");
     delay(1000);
     String yollanacakkomut = "GET /update?key=JT2XYKVRYAQOEQAK&field1=";
@@ -48,5 +44,5 @@ void sicaklik_yolla(float sicaklik){
     delay(2000);
     Serial.print(yollanacakkomut);
     Serial.print("\r\n\r\n");
-
-}
+  }
+  
