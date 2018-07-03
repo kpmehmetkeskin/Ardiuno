864ef3af2f27718b1a2b517593aff974a89676df
kpmehmetkeskin <mehmet.keskin@koctas.com.tr>
2018-07-03 16:26:48 +0300

    initial commit

---
+++ home_temp/home_temp.ino
@@ -0,0 +1,52 @@
+#include <DHT.h>
+#include <Adafruit_Sensor.h>
+
+#define ag_ismi "JAVA_HOME"
+#define ag_sifresi "avcz3463"
+#define IP "184.106.153.149"    //thingspeak.com IP adresi
+
+#define DHTPIN 7     // what pin we're connected to
+#define DHTTYPE DHT22   // DHT 22  (AM2302)
+DHT dht(DHTPIN, DHTTYPE);
+
+
+ 
+  float sicaklik;
+ 
+  void setup()
+  {
+  Serial.begin(115200);
+  dht.begin();
+  Serial.println("AT");
+  delay(1000); 
+  analogReference(INTERNAL);
+    if(Serial.find("OK")){
+      Serial.println("AT+CWMODE=1"); 
+      delay(1000);
+      String baglantiKomutu=String("AT+CWJAP=\"")+ag_ismi+"\",\""+ag_sifresi+"\"";
+      Serial.println(baglantiKomutu);
+    }
+  }
+ 
+void loop(){
+  float sicaklik = dht.readTemperature();
+ Serial.println(sicaklik);
+ sicaklik_yolla(sicaklik);
+ delay(10000);
+}
+ 
+ 
+void sicaklik_yolla(float sicaklik){
+    Serial.println(String("AT+CIPSTART=\"TCP\",\"") + IP + "\",80");
+    delay(1000);
+    String yollanacakkomut = "GET /update?key=JT2XYKVRYAQOEQAK&field1=";
+    yollanacakkomut += (int(sicaklik));
+    yollanacakkomut += "\r\n\r\n";
+    delay(2000);
+    Serial.print("AT+CIPSEND=");
+    Serial.println(yollanacakkomut.length()+2);
+    delay(2000);
+    Serial.print(yollanacakkomut);
+    Serial.print("\r\n\r\n");
+
+}
